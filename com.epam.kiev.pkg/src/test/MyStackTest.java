ee406f247cbc047851d20e852e307e47ce3be47a
YVelb <Yana_Velbovets@epam.com>
2014-06-24 15:32:08 +0300

    Updated MyStack test

--- com.epam.kiev.pkg/src/test/MyStackTest.java
+++ com.epam.kiev.pkg/src/test/MyStackTest.java
@@ -11,71 +11,79 @@ import com.epam.kiev.pkg.MyStack;
 
 public class MyStackTest {
 	MyStack Stacktest;
-	
 
-@Before
-public void Set(){
-	Stacktest = new MyStack();
-	
-}
-
-@Test(expected=IndexOutOfBoundsException.class)
-public void testPushExept() {
-	int i = 15;
-	for (int count = 0; count < i; count++){
-	Stacktest.push(count);
+	@Before
+	public void Set() {
+		Stacktest = new MyStack();
 	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testPushExept() {
+		Stacktest.push(0);
+		Stacktest.push(1);
+		Stacktest.push(2);
+		Stacktest.push(3);
+		Stacktest.push(4);
+		Stacktest.push(5);
+		Stacktest.push(6);
+		Stacktest.push(7);
+		Stacktest.push(8);
+		Stacktest.push(9);
+		Stacktest.push(10);
+	}
+
+	@Test
+	public void testPush() {
+		Stacktest.push(0);
+		Stacktest.push(1);
+		Stacktest.push(2);
+		Stacktest.push(3);
+		Stacktest.push(4);
+		Stacktest.push(5);
+		Stacktest.push(6);
+		assertEquals(6, Stacktest.peek(), 0.1);
+		System.out.println(Stacktest.peek() + " - Stack peek after creation");
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testPeekExcept() {
+		Stacktest.empty();
+		Stacktest.peek();
+	}
+
+	@Test
+	public void testPeek() {
+		Stacktest.push(0);
+		Stacktest.push(1);
+		Stacktest.push(2);
+		Stacktest.push(3);
+		Stacktest.push(4);
+		assertEquals(4, Stacktest.peek(), 0.1);
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testPopExcept() {
+		Stacktest.empty();
+		Stacktest.pop();
+	}
+
+	@Test
+	public void testPop() {
+		Stacktest.push(0);
+		Stacktest.push(1);
+		Stacktest.push(2);
+		Stacktest.push(3);
+		Stacktest.push(4);
+		Stacktest.push(5);
+		Stacktest.push(6);
+		Stacktest.push(7);
+		Stacktest.pop();
+		assertEquals(6, Stacktest.peek(), 0.1);
+	}
+
+	@Test
+	public void testEmpty() {
+		assertTrue("0", Stacktest.empty());
+	}
+
 }
-
-@Test
-public void testPush() {
-	int i = 7;
-	for (int count = 0; count < i; count++){
-		Stacktest.push(count);
-		}
-	String inttostr = Stacktest.peek() + "";
-	assertEquals(inttostr, "6");
-	System.out.println(Stacktest.peek() + " - Stack peek after creation");
-}
-
-@Test(expected=EmptyStackException.class)
-public void testPeekExcept() {
-	Stacktest.empty();
-	Stacktest.peek();
-}
-
-@Test
-public void testPeek() {
-	int i = 5;
-	for (int count = 0; count < i; count++){
-		Stacktest.push(count);
-		}
-	String inttostr = Stacktest.peek() + "";
-	assertEquals(inttostr, "4");
-}
-
-@Test(expected=EmptyStackException.class)
-public void testPopExcept() {
-	Stacktest.empty();
-	Stacktest.pop();	
-}
-
-@Test
-public void testPop() {
-	int i = 8;
-	for (int count = 0; count < i; count++){
-		Stacktest.push(count);
-		}
-	Stacktest.pop();
-	String inttostr = Stacktest.peek() + "";
-	assertEquals(inttostr, "6");
-}
-
-@Test
-public void testEmpty(){
-	assertTrue("0", Stacktest.empty());
-}
-
-}
-
-
